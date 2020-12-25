08e60b1885a32e682b5038ec129b42a2105e7b58
juhodev <juho.seppl@gmail.com>
2020-12-25 13:39:45 +0200

    Add comments

--- caching/lfu.py
+++ caching/lfu.py
@@ -48,12 +48,13 @@ class LFU:
             self._remove_least_used()
         self.size += 1
         print('insert', key)
-        # when the first element is added, a single element in the hash map
-        # is created which points to this new element (by its key) and new
-        # frequency node with a value of 1 is added to the frequency list
         if self.frequency_list.size == 0:
+            # if the frequency_list is empty create a new frequency node with the
+            # frequency of one
             self._create_frequency_node(key, value, 1)
         else:
+            # if there already is a frequency node then insert the new
+            # node into it
             n = Node(key, value, self.frequency_list.head)
             self.frequency_list.head.children.insert_head(n)
             self.lookup[key] = n
@@ -68,28 +69,40 @@ class LFU:
         return n.value
 
     def _increment(self, node, current_parent):
+        # this is the next node in the frequency_list
         next_node = current_parent.next
+        # remove the node that I'm about to increment from the it's current parent
+        # because it's about to be moved
         current_parent.children.remove(node)
         if next_node == None or next_node.value != current_parent.value+1:
+            # If there is not a frequency node with a frequency value of the current + 1 then create a new
+            # frequency node
             self._create_frequency_node(
                 node.key, node.value, current_parent.value+1, current_parent)
         else:
+            # if there is a frequency node with the correct frequency value then
+            # I just need to create a new node with that frequency node as the parent
+            # and update ther lookup dict
             n = Node(node.key, node.value, next_node)
             self.lookup[node.key] = n
             next_node.children.insert_head(n)
 
     def _create_frequency_node(self, key, node_value, frequency, old_parent=None):
         frequency_node = Node('frequency-node', frequency)
+        # create a list node with the frequency node as it's parent
         list_node = Node(key, node_value, frequency_node)
         frequency_node.children.insert_head(list_node)
 
         self.lookup[key] = list_node
         if old_parent == None:
+            # the old_parent is going to None when inserting the key, value for the first time
             self.frequency_list.insert_head(frequency_node)
         else:
             old_parent.next = frequency_node
 
     def _remove_least_used(self):
+        # first get the left most frequency node (it'll be the least used nodes) and
+        # then remove any child from that node
         least_used_node = self.frequency_list.head
         head = least_used_node.children.head
         least_used_node.children.remove(head)
